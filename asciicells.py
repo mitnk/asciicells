c3e5c806eabc1733d8d7bd0f0d3dfc5962951710
Hugo Wang <w@mitnk.com>
2015-11-20 13:32:18 +0800

    almost works

--- asciicells.py
+++ asciicells.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict
 import itertools
 
@@ -5,6 +6,8 @@ import itertools
 CROSS = '+'
 HORIZ = '-'
 VERTI = '|'
+MAX_WIDTH = 72
+MIN_CELL_WIDTH = 10
 
 def get_width_info(L):
     info = defaultdict(int)
@@ -12,8 +15,50 @@ def get_width_info(L):
         for item, i in zip(row, itertools.count()):
             if len(item) > info[i]:
                 info[i] = len(item)
-    info['width'] = sum(info.values()) + len(info) * 3 + 1
+    width_extra = len(info) * 3 + 1
+    info['width'] = sum(info.values()) + width_extra
+    if info['width'] > MAX_WIDTH:
+        info['width'] = MAX_WIDTH
+
+    index_to_wrap = itertools.count()
+    while sum([info[i] for i in range(len(L[0]))]) > info['width'] - width_extra:
+        index = index_to_wrap.__next__() % len(L[0])
+        if info[index] <= MIN_CELL_WIDTH:
+            continue
+        info[index] -= 1
     return info
+
+
+def _get_empty_row(token):
+    return re.sub(r'[^{}]'.format(VERTI), ' ', token)
+
+
+def _split_column_str(column, width):
+    return column[:width], column[width:]
+
+
+def _get_normal_row(item_list, width_info, orphans):
+    token = VERTI
+    if isinstance(item_list, defaultdict):
+        item_list = [item_list[i] for i in range(len(item_list))]
+    for item, i in zip(item_list, itertools.count()):
+        if len(item) > width_info[i]:
+            padding = ''
+            str_curr, str_left = _split_column_str(item, width_info[i])
+            orphans[i] = str_left
+        else:
+            padding = ' ' * (width_info[i] - len(item))
+            str_curr = item
+            orphans[i] = ''
+        token += ' {}{} {}'.format(str_curr, padding, VERTI)
+    return token
+
+
+def _are_orphans_left(orphans):
+    for k in orphans:
+        if len(orphans[k]) > 0:
+            return True
+    return False
 
 
 def get_table(L):
@@ -21,12 +66,17 @@ def get_table(L):
     width = info_w['width']
 
     rows = []
+    orphans = defaultdict(str)
     for item_list in L:
-        token = VERTI
-        for item, i in zip(item_list, itertools.count()):
-            padding = ' ' * (info_w[i] - len(item))
-            token += ' {}{} {}'.format(item, padding, VERTI)
+        token = _get_normal_row(item_list, info_w, orphans)
         rows.append(token)
+        while _are_orphans_left(orphans):
+            token = _get_normal_row(orphans, info_w, orphans)
+            rows.append(token)
+        rows.append(_get_empty_row(token))
+
+    rows.pop()  # pop out the last empty row
+
     cross_indexes = set([])
     for c, i in zip(rows[0], itertools.count()):
         if c == VERTI:
@@ -43,5 +93,23 @@ def get_table(L):
 
 
 if __name__ == '__main__':
-    L = list(itertools.permutations(['1', 'ascii', 'this is a test'], 3))
-    print(get_table(L))
+    L1 = list(itertools.permutations(['1', 'ascii', 'this is a test'], 3))
+    L2 = [
+        [
+            'it must be continuously available',
+            'In 2011, millions of people began to use the China Railways '
+                'website on a daily basis and the system began experiencing '
+                'problems with performance.',
+            'Extra',
+            'N/A',
+        ],
+        [
+            'his team performed a root cause',
+            'the old system: the relational database was overloaded and '
+                'could not handle either the scale of incoming requests or '
+                'the level of reliability.',
+            'Python',
+            '-',
+        ],
+    ]
+    print(get_table(L2))
